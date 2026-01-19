4bdcdf6ec2d21e78dca6d76cacabe79eb9d615f0
Omar Ansari <omarahmedansari@gmail.com>
2026-01-18 21:18:18 -0500

    fix

--- portal/next.config.js
+++ portal/next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      // Proxy Clerk's internal API routes
-      {
-        source: '/api/__clerk/:path*',
-        destination: 'https://clerk.subscribersync.com/npm/:path*',
-      },
-      {
-        source: '/clerk/:path*',
-        destination: 'https://clerk.subscribersync.com/:path*',
-      },
-    ]
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
